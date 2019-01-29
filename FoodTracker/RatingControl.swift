763a284932339c9cb5f3fabdb3d1dd406bab6bdb
Terashima <yamanogomora@gmail.com>
2019-01-29 10:11:11 +0900

    レーティングのカスタム View を 5個配置する

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -27,16 +27,29 @@ class RatingControl: UIStackView {
     
     // MARK: Private Methods
     private func setupButtons() {
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        // Add the button to the stack
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to rating button array
+            ratingButtons.append(button)
+        }
     }
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
 }
