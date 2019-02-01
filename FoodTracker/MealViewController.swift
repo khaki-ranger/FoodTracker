6b742c1213a1f66f1a4aaf03e3fbbd323f087f94
Terashima <yamanogomora@gmail.com>
2019-02-01 11:21:34 +0900

    詳細画面の Cancel ボタンの挙動を実装

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -40,6 +40,11 @@ class MealViewController: UIViewController {
     
     //MARK: Navigation
     // This method lets you configure a view controller before it's presented.
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
